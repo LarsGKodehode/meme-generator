02010bc5d46e9e5e9dd21a10916b2799a86d0242
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-07-20 11:52:24 +0200

    intitial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A simple meme generator implemented as a single page app utilizing React and built using Vite
---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Components
+import App from './App';
+
+// CSS
+import './assets/css/global.css';
+import './assets/css/palettes.css';
+import './assets/css/typography.css';
+import './index.css';
+// Fonts
+import './assets/fonts/StintUltraCondensed-Regular.ttf';
+
+
+/**
+ * DEVELOPMENT
+ * clears console of error messages between runs
+ * @link https://github.com/vitejs/vite/discussions/3143
+ */ 
+ if (import.meta.hot) {
+  import.meta.hot.on(
+    "vite:beforeUpdate",
+    () => console.clear()
+  );
+}
+// DEVELOPMENT
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
