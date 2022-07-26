7ebff19a0f631e9f1c1a4c1e95e769b98fe7988c
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-07-26 09:27:00 +0200

    moved props definition into component definition

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,31 @@ import Input from './assets/components/Input/Input';
 import './App.css';
 
 
-// ===== Local props definition =====
-
-// header
 // SVG Path
 import svgURL from './assets/images/Peace.svg';
-
-const headerProps = {
-  text: 'Meme Generator',
-  svg: {
-    data: svgURL, // Path to svg
-    type: 'image/svg+xml',
-    },
-};
-
-// Output
 // Image Path
 import imagePath from './assets/images/meme-sample.jpg';
-
-const outputProps = {
-  textTop: 'ES6',
-  textBottom: `But it ain't Neurosurgery`,
-  imageURL: imagePath,
-  memeAlt: `ES6 might be hard to learn, but there exists stuff that's harder`,
-};
 
 
 // COMPONENT
 function App() {
+  // ===== Local props definition =====
+  // header
+  const headerProps = {
+    text: 'Meme Generator',
+    svg: {
+      data: svgURL, // Path to svg
+      type: 'image/svg+xml',
+      },
+  };
+
+  // Output
+  const outputProps = {
+    textTop: 'ES6',
+    textBottom: `But it ain't Neurosurgery`,
+    imageURL: imagePath,
+    memeAlt: `ES6 might be hard to learn, but there exists stuff that's harder`,
+  };
 
   return (
     <main className='App' data-theme="bright">
